7358c3d6c8822c5771b3ef963863c202eaac37d0
cayomol01 <jacmg01@gmail.com>
2023-04-23 17:51:55 -0600

    done

--- TreeDFA.py
+++ TreeDFA.py
@@ -10,15 +10,37 @@ def TreeToDFA(regex):
     s = regex
     
     root = CreateSyntaxTree(s)
-    root.ShowGraph()
-
+    #root.ShowGraph()
+    print(" ")
     followpos_table = root.getTable()  
     for i in followpos_table:
-        print(i)  
+        print(i) 
+    print(" ")
+        
+    followpos_copy =followpos_table.copy()
+        
+    new_followpos = []
+    ''' while followpos_copy:
+        temp = followpos_copy.pop(0)
+        new_followpos.append(temp)
+        num = temp[1]
+        #print(new_followpos)
+        for i in range((num), len(followpos_table)):
+            if followpos_table[i][2] == temp[2]:
+                new_followpos[num-1][2].add(followpos_table[i][1])
+                #print('aaa', followpos_table[i])
+                #print('bbb', new_followpos)
+                #followpos_copy.remove(followpos_table[i]) '''
+                
+    #print(new_followpos)
+    #print(followpos_table)
+    
+    
+    
     
     # Encontramos el alfabeto de la tabla
-    alphabet = set(entry[0] for entry in followpos_table if entry[0] != "#")
-    print(alphabet)
+    alphabet = list(set(entry[0] for entry in followpos_table if entry[0] != "#"))
+    #print(alphabet)
     final = followpos_table[-1][1]
     initial =followpos_table[0][1]
     
@@ -26,7 +48,7 @@ def TreeToDFA(regex):
     stack = [followpos_table[0][2]]
     looked = [followpos_table[0][2]]
     
-    
+    stack.append(followpos_table[1][2])
     while stack:
         look_at = stack.pop()
         #print(alphabet)
@@ -35,12 +57,21 @@ def TreeToDFA(regex):
             for node in look_at:
                 if followpos_table[node-1][0] == letter:
                     nodos.update(followpos_table[node-1][2])
-            if nodos not in looked:
+            if nodos not in looked and nodos:
                 looked.append(nodos)
                 stack.append(nodos)
             trans.append((letter, look_at, nodos))
-            print(looked)
+            
+            #print(looked)
+            
+    print(trans)
+    print(nodos)       
     
+    
+
+    
+    print(trans)
+    print("\naaaaaaa\n")
     transition = [trans[i] for i in range(len(trans)) if (trans[i][1] and trans[i][2])]
 
     print(transition)
@@ -88,6 +119,104 @@ def TreeToDFA(regex):
 
 
 
+a = TreeToDFA('(a|b)')
+a.ShowGraph2(name="pruebas/prueba")
+def construct_states(regex):
+    s = regex
+    
+    root = CreateSyntaxTree(s)
+    #root.ShowGraph()
+
+    followpos_table = root.getTable()  
+    for i in followpos_table:
+        print(i) 
+        
+
+    
+    # Encontramos el alfabeto de la tabla
+    alphabet = list(set(entry[0] for entry in followpos_table if entry[0] != "#"))
+    #print(alphabet)
+    final = followpos_table[-1][1]
+    initial =followpos_table[0][1]
+    
+    trans = [followpos_table[0]]
+    stack = [followpos_table[0][2]]
+    looked = [followpos_table[0][2]]
+    
+    stack.append(followpos_table[1][2])
+    
+    #recorrer el 
+    
+    while stack:
+        look_at = stack.pop()
+        #print(alphabet)
+        for letter in alphabet:
+            nodos = set()
+            for node in look_at:
+                if followpos_table[node-1][0] == letter:
+                    nodos.update(followpos_table[node-1][2])
+            if nodos not in looked and nodos:
+                looked.append(nodos)
+                stack.append(nodos)
+            trans.append((letter, look_at, nodos))
+            
+            #print(looked)
+            
+    print(trans)
+    print(nodos)       
+
+    print(trans)
+    print("\naaaaaaa\n")
+    transition = [trans[i] for i in range(len(trans)) if (trans[i][1] and trans[i][2])]
+
+    print(transition)
+    names = {}
+    count = 0
+    for i in range(len(transition)):
+        a = transition[i][1]
+        b = transition[i][2]
+        if type(a) == int:
+            a = set({a})
+        if type(b) == int:
+            b = set({b})
+        if frozenset(a) not in names.keys():
+            names[frozenset(a)] = "S"+str(count)
+            count+=1
+        if frozenset(b) not in names.keys():
+            names[frozenset(b)] = "S"+str(count)
+            count+=1
+         
+    transitions = {}  
+
+    for i in range(len(transition)):
+        a = transition[i][1]
+        b = transition[i][2]
+        if type(a) == int:
+            a = set({a})
+        if type(b) == int:
+            b = set({b})
+        if names[frozenset(a)] not in transitions.keys():
+            transitions[names[frozenset(a)]] = {transition[i][0]: names[frozenset(b)]}
+        else:
+            transitions[names[frozenset(a)]].update({transition[i][0]: names[frozenset(b)]})
+
+    print(transitions)
+    start_state = names[frozenset({initial})]
+    accepting_states = []
+    for key, value in names.items():
+        if final in key and value not in accepting_states:
+            accepting_states.append(value)
+   
+    
+    # Inicializamos el estado inicial que sería el followpos de nuestro root.
+    
+    return AFD(start_state, accepting_states, transitions)
+
+
+
+#followpos_table = [('a', 1, {3}), ('b', 2, {3}), ('#', 3, set())]
+#dfa = construct_states(followpos_table)
+#print(dfa.transitions)
 
 #Revisar estado de inicio, aka: estado 1
 
